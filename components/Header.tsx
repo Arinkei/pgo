a8887f9987e86f76687ab2492d1b602268a632fe
Arinkei <arinkei@naver.com>
2025-03-18 17:21:07 +0900

    md

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,13 @@
-//"use client"; // Next.js 13+에서는 이벤트 핸들링을 위해 클라이언트 컴포넌트로 설정
+"use client"; // Next.js 13+에서는 이벤트 핸들링을 위해 클라이언트 컴포넌트로 설정
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Header() {
-    //const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <header className="fixed w-full top-0 z-50 !bg-blue-300 dark:bg-gray-900 p-4 h-16">
+        <header className="fixed w-full top-0 z-50 !bg-blue-300 p-4 h-16">
             <div className="container mx-auto flex justify-center items-center">
                 {/* 로고 */}
                 <Link href="/">
