5d7e99e9cbc29e1ca3e57164c10667fc789348c5
Arinkei <arinkei@naver.com>
2025-03-18 16:36:05 +0900

    md

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <header className="fixed w-full top-0 z-50 bg-blue-300 dark:bg-gray-900 p-4 h-16">
+        <header className="fixed w-full top-0 z-50 !bg-blue-300 dark:bg-gray-900 p-4 h-16">
             <div className="container mx-auto flex justify-center items-center">
                 {/* 로고 */}
                 <Link href="/">
