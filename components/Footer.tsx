fd666b6735009bd6eb65af96876e755d7ba465e0
Arinkei <arinkei@naver.com>
2025-03-19 11:25:55 +0900

    md

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,16 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-        <footer className="w-full z-50 !bg-gray-50 p-4 h-30">
+        <footer className="w-full z-50 bg-gray-50 p-4 h-30">
             <div className="container mx-auto !bg-gray-50 flex justify-center items-center">
                 ⓒ 2025. PGO All rights reserved
+            </div>
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                마지막 업데이트&nbsp;
+                <code className="font-mono font-bold">'${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, '0')}-${String(new Date().getDate
+                ()).padStart(2,'0')}'</code> 
+                </p>
             </div>
         </footer>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-between">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          마지막 업데이트&nbsp;
-          <code className="font-mono font-bold">2025-03-13</code> 
-        </p>
-      </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <Link
