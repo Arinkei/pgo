10a2a05d8331ccfb1ef4b03916086e476a351237
Arinkei <arinkei@naver.com>
2025-03-21 17:59:28 +0900

    md

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,13 +21,13 @@ export default function Footer() {
                 ⓒ 2025. PGO All rights reserved
                 </div>
             </div>
-            <div className="fixed bottom-2 right-4 w-64">
+            {/* <div className="bottom-2 right-4 w-64">
                 <p className="border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-2xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                 마지막 업데이트&nbsp;
                 <code className="font-mono font-bold">{new Date().getFullYear()}-{String(new Date().getMonth() + 1).padStart(2, '0')}-{String(new Date().getDate
                 ()).padStart(2,'0')}</code>
                 </p>
-            </div>
+            </div> */}
         </footer>
     );
 }
